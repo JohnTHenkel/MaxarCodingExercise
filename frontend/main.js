750e656cc0403ddfb903ab709691df78b269e301
JohnTHenkel <JohnTHenkel@gmail.com>
2021-12-19 11:55:46 -0700

    Highlight area of interest on map with data retrieved from backend.

--- frontend/main.js
+++ frontend/main.js
@@ -2,13 +2,23 @@ import './style.css';
 import {Map, View} from 'ol';
 import TileLayer from 'ol/layer/Tile';
 import OSM from 'ol/source/OSM';
+import VectorLayer from 'ol/layer/Vector';
+import VectorSource from 'ol/source/Vector';
+import GeoJSON from 'ol/format/GeoJSON';
+
 
 const map = new Map({
   target: 'map',
   layers: [
     new TileLayer({
       source: new OSM()
-    })
+    }),
+    new VectorLayer({
+      source: new VectorSource({
+        format: new GeoJSON(),
+        url: "http://localhost:8080/data"
+      })
+    }),
   ],
   view: new View({
     center: [0, 0],
