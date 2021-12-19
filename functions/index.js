6d1add244f000e547cb9e5160ac63f760f0219fd
JohnTHenkel <JohnTHenkel@gmail.com>
2021-12-19 12:19:36 -0700

    Refactored backend to use firebase functions.
    Migrated from local server to serverless approach for rest API.
    Updated code used by frontend accordingly.

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,18 @@
 const functions = require("firebase-functions");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+const fs = require('fs');
+
+const app = express()
+app.use(cors())
+
+const data = fs.readFileSync(path.resolve('./Wellington_21Q3_V0_AOI.geojson'))
+const jsonData = JSON.parse(data);
+
+app.get('/data', (req,res) => {
+    res.status(200).send(jsonData)
+})
+
+exports.app = functions.https.onRequest(app)
