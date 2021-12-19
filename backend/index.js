6aa158314ce1a13b213e94b83b41d4285116c4f1
JohnTHenkel <JohnTHenkel@gmail.com>
2021-12-19 11:33:55 -0700

    Read data from json file at startup; send data upon request to /data endpoint.

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const fs = require('fs');
+
 const PORT = 8080
 const app = express()
 
+const data = fs.readFileSync('Wellington_21Q3_V0_AOI.geojson')
+const jsonData = JSON.parse(data);
+
 app.get('/data', (req,res) => {
-    res.status(200).send("Hello, world!")
+    res.status(200).send(jsonData)
 })
 
 app.listen(PORT)
